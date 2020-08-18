97a44cba23e316b45516223b1e7038c604db5ff8
saritalkadaa <42039718+saritalkadaa@users.noreply.github.com>
2020-08-18 09:30:49 -0400

    Create home.js

---
+++ cuny-front-end/src/home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class Home extends React.Component {
+    render(){
+        return(
+            <div>
+<div  className= "homePage" id="home">
+<br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+  <h1 className="display-3" >CUNY Startup's Connecter</h1>
+  <hr/>
+  <h5 id="headline">Find your match today!</h5>
+  <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+  </div>
+            </div>
+        );
+    }
+}
+export default Home;
