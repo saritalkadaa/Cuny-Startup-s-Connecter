72d80e0cd4af063148baa9828c58b662f2aa92bb
saritalkadaa <42039718+saritalkadaa@users.noreply.github.com>
2020-08-18 13:03:33 -0400

    Create about.js

---
+++ cuny-front-end/src/about.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class About extends React.Component {
+    render(){
+        return(
+            <div>
+<div  className= "homePage" id="aboutUs">
+<br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+  <h5>Our Goal is to connect CUNY STEM students to liberal arts students in order to facilitate entrepreneurship by combining technical skills with project ideas.</h5>
+
+  <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+  </div>
+            </div>
+        );
+     }
+}
+export default About;
